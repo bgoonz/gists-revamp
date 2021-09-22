50a5795d79c53c0ccdcff990114a21b86ee30974
Bryan C Guner <bryan.guner@gmail.com>
2021-09-22 18:52:14 +0000

    -update

--- ARCHIVE/PYTHON-DS/_CODESIGNAL/my/firstDuplicate.py
+++ ARCHIVE/PYTHON-DS/_CODESIGNAL/my/firstDuplicate.py
@@ -15,6 +15,7 @@
 
 # # print(firstDuplicate(arr))
 
+arr = [1,2,3,4,5,6,7,8]
 
 def firstDuplicate(a):
     nums = set()
@@ -26,5 +27,5 @@ def firstDuplicate(a):
     return -1
 
 
-# print(firstDuplicate(arr))
+ print(firstDuplicate(arr))
 
