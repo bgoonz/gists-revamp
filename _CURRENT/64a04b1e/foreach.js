ae7c226e29154b988d892c9e65200367b9758ce0
bgoonz <bryan.guner@gmail.com>
2021-09-22 07:54:21 +0000

    -update

--- _CURRENT/64a04b1e/foreach.js
+++ _CURRENT/64a04b1e/foreach.js
@@ -1,11 +1,11 @@
-let data = [ 1, 2, 3, 4, 5 ];
-  let sum = 0;
-data.forEach( value => {
+let data = [1, 2, 3, 4, 5];
+let sum = 0;
+data.forEach((value) => {
   sum += value;
-} );
-data.forEach( function ( v, i, a ) {
-  a[ i ] = v + 1;
-} );
+});
+data.forEach(function (v, i, a) {
+  a[i] = v + 1;
+});
 
-console.log("🚀 data", data, "sum" ,sum)
+console.log("🚀 data", data, "sum", sum);
 //🚀 data [ 2, 3, 4, 5, 6 ] sum 15
