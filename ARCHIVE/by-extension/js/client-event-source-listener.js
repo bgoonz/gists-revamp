68f3bc3332cf63c504cef3856d546bc30c62b08b
bgoonz <bryan.guner@gmail.com>
2021-09-22 08:54:47 +0000

    -update

--- ARCHIVE/by-extension/js/client-event-source-listener.js
+++ ARCHIVE/by-extension/js/client-event-source-listener.js
@@ -1,19 +1,19 @@
 if (!!window.EventSource) {
-  const source = new EventSource('/api/bubble-sort');
+  const source = new EventSource("/api/bubble-sort");
   source.onmessage = (e) => {
     const data = JSON.parse(e.data);
     // data parsed
   };
 
   source.onopen = () => {
-    console.log('connected!');
+    console.log("connected!");
   };
 
   source.onerror = (e) => {
     if (e.target.readyState === EventSource.CLOSED) {
-      console.log('closed');
+      console.log("closed");
     } else if (e.target.readyState === EventSource.CONNECTING) {
-      console.log('trying to connect');
+      console.log("trying to connect");
     }
   };
 } else {
--- ARCHIVE/by-extension/js/cluster.js
+++ ARCHIVE/by-extension/js/cluster.js
@@ -1,7 +1,6 @@
-const cluster = require('cluster');
-const http = require('http');
-const numCPUs = require('os').cpus().length;
-
+const cluster = require("cluster");
+const http = require("http");
+const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
@@ -11,16 +10,18 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
+  cluster.on("exit", (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
   // Workers can share any TCP connection
   // In this case it is an HTTP server
-  http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end('hello world\n');
-  }).listen(8000);
+  http
+    .createServer((req, res) => {
+      res.writeHead(200);
+      res.end("hello world\n");
+    })
+    .listen(8000);
 
   console.log(`Worker ${process.pid} started`);
 }
