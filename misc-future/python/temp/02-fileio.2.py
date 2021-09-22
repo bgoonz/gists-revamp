17bd243ca9da78f7dc313a717a86e37207f50601
bgoonz <bryan.guner@gmail.com>
2021-09-22 10:30:39 +0000

    -update

--- misc-future/python/temp/02-fileio.2.py
+++ misc-future/python/temp/02-fileio.2.py
@@ -1,9 +1,10 @@
 # argv and argc take in command line args
 import sys
+
 if len(sys.argv) != 2:
     print("usage: 02-fileio.2.py <filename>")
     sys.exit(1)
-try:  
+try:
     with open(sys.argv[1]) as f:
         for line in f:
             print(line)
