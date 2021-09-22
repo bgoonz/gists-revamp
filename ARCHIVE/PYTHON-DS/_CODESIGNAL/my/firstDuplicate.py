dbc5a86963c67952f07578fb7651f106605ec77f
Bryan C Guner <bryan.guner@gmail.com>
2021-09-22 18:51:48 +0000

    -update

---
+++ ARCHIVE/PYTHON-DS/_CODESIGNAL/my/firstDuplicate.py
@@ -0,0 +1,30 @@
+# # O(n) solution
+# def firstDuplicate(a):
+#     # define a set to hold non duplicate numbers
+#     nums = set()
+#     for i in range(len(a)):
+#         # if we hit a number that's duplicate (exists in the set) return it
+#         if a[i] in nums:
+#             return a[i]
+#         else:
+#             # else if its not duplicate add it to the set
+#             nums.add(a[i])
+#     # return -1 if no duplicates
+#     return -1
+
+
+# # print(firstDuplicate(arr))
+
+
+def firstDuplicate(a):
+    nums = set()
+    for i in range(len(a)):
+        if a[i] in nums:
+            return a[i]
+        else:
+            nums.add(a[i])
+    return -1
+
+
+# print(firstDuplicate(arr))
+
