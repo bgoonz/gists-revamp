6a1512446c0f717749ef78f229b1047914861199
Bryan C Guner <bryan.guner@gmail.com>
2021-09-22 18:54:25 +0000

    -update

--- ARCHIVE/PYTHON-DS/_CODESIGNAL/my/firstDuplicate.py
+++ ARCHIVE/PYTHON-DS/_CODESIGNAL/my/firstDuplicate.py
@@ -16,6 +16,14 @@
 # # print(firstDuplicate(arr))
 
 
+# 1. We create a set to store the numbers we’ve seen so far.
+# 2. We loop through the array.
+# 3. If the current number is in the set, we return it.
+# 4. Otherwise, we add the current number to the set.
+# 5. If we make it to the end of the loop, we return -1.
+
+
+
 
 arr = [1,2,3,4,5,6,7,8]
 arr2 = [1,2,3,4,5,6,7,8,8]
