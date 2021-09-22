d241cbdceb321416b807aca70d769da754a93aaa
bgoonz <bryan.guner@gmail.com>
2021-09-22 07:58:52 +0000

    -update

--- _CURRENT/c8d30499/underscore-2-camel.js
+++ _CURRENT/c8d30499/underscore-2-camel.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const glob = require('glob');
-const recast = require('recast');
+const fs = require("fs");
+const glob = require("glob");
+const recast = require("recast");
 const types = recast.types;
 
 function compile(filepath) {
@@ -14,17 +14,16 @@ function compile(filepath) {
         name = name.replace(/_(.)/g, (match, char, offset) => {
           return offset === 0 ? match : char.toUpperCase();
         });
-        path.get('name').replace(name);
+        path.get("name").replace(name);
       }
       return false;
-    }
+    },
   });
 
   fs.writeFileSync(filepath, recast.print(ast).code);
 }
 
-
-glob('src/**/*.js', (err, files) => {
+glob("src/**/*.js", (err, files) => {
   if (err) {
     console.log(err);
     process.exit(1);
